11d089c0cb78229449e67f14a90da26271e2fbd2
Nishantvidhuri <nishantvidhuri098&@gmail.com>
2025-02-05 18:16:42 +0530

    modified:   package-lock.json

--- src/pages/Routes.jsx
+++ src/pages/Routes.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import Location from './Location';
 import Services from './Services';
+import QA from './QandA';
 
 function AppRoutes() { // Rename to avoid conflict with React Router's Routes
   return (
@@ -10,6 +11,8 @@ function AppRoutes() { // Rename to avoid conflict with React Router's Routes
       <Route path="/" element={<Home />} />
       <Route path="/location" element={<Location />} />
       <Route path="/services" element={<Services />} />
+      <Route path="/q-a" element={<QA />} />
+      
 
     </Routes>
   );
